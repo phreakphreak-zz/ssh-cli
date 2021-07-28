67f25459f32d086b78e07ce849bb0fe26c5fcc5c
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2021-07-28 04:57:59 -0400

    feat: add compoment generate to program initial

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,41 @@
 import { Command } from 'commander';
+import { generateCommand } from './commands/generate';
 
 // const program = new Command();
 
 // program.parse(process.argv);
 const program = new Command();
 
-const fooCommand = new Command('foo');
-const barCommand = new Command('bar');
-const yepCommand = new Command('yep');
+// const fooCommand = new Command('foo');
+// const barCommand = new Command('bar');
+// const yepCommand = new Command('yep');
 
-yepCommand
-    .argument('[arg]')
-    .option('-o, --opt <type>')
-    .description('yep bar')
-    .action((args, options) => {
-        console.log(args, options);
-    });
+// yepCommand
+//     .argument('[arg]')
+//     .option('-o, --opt <type>')
+//     .description('yep bar')
+//     .action((args, options) => {
+//         console.log(args, options);
+//     });
 
-fooCommand
-    .argument('<arg>')
-    .description('des bar')
-    .action((arg) => {
-        console.log(arg);
-    });
+// fooCommand
+//     .argument('<arg>')
+//     .description('des bar')
+//     .action((arg) => {
+//         console.log(arg);
+//     });
 
-barCommand
-    .argument('<arg>')
-    .description('des bar')
-    .action((arg) => {
-        console.log(arg);
-    });
+// barCommand
+//     .argument('<arg>')
+//     .description('des bar')
+//     .action((arg) => {
+//         console.log(arg);
+//     });
 
-program.addCommand(fooCommand);
-program.addCommand(barCommand);
-program.addCommand(yepCommand);
+// program.addCommand(fooCommand);
+// program.addCommand(barCommand);
+// program.addCommand(yepCommand);
+
+program.addCommand(generateCommand);
 
 program.parse(process.argv);
